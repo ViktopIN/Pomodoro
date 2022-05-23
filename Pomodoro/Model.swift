790cb2b7e3fc9ae0cc71985290ee0ccb730d182f
Viktor Pinyagin <viktopiniaginya@gmail.com>
2022-05-23 23:13:07 +0300

    create model again

--- Pomodoro/Model.swift
+++ Pomodoro/Model.swift
@@ -2,7 +2,56 @@
 //  Model.swift
 //  Pomodoro
 //
-//  Created by t h a . m a m e rozz on 23.05.2022.
+//  Created by t h a . m a m e rozz on 21.05.2022.
 //
 
-import Foundation
+import UIKit
+
+extension ViewController {
+    
+// MARK: - Extra methods
+    
+    func secondConverter(_ seconds: Int) -> String {
+            
+        let formatter = DateComponentsFormatter()
+        
+        formatter.allowedUnits = [.minute, .second]
+        
+        let formattedString = formatter.string(from: TimeInterval(seconds))!
+            
+        return formattedString
+    }
+    
+    func refreshButton() {
+        
+        button.isSelected = false
+        button.isHighlighted = false
+        
+        self.timerLabel.text = self.secondConverter(Metric.workModeDuration)
+        self.timerLabel.textColor = self.mainColor
+        
+        DispatchQueue.main.async {
+
+            self.circleToggle.timerDuration = Metric.workModeDuration
+            self.circleToggle.miniCircleView.layer.borderColor = self.mainColor.cgColor
+            self.circleToggle.miniFilledCircleView.backgroundColor = self.mainColor
+            
+            self.circleToggle.stopAnimation()
+            
+            self.shapeLayer.strokeEnd = 1
+            self.shapeLayer.removeAllAnimations()
+            
+            self.durationTimer = Metric.workModeDuration
+        }
+        
+    }
+    
+    func choosenColor(value: Bool) -> UIColor {
+        
+        guard value else { return ViewController.Colors.restColor }
+        return ViewController.Colors.workColor
+        
+    }
+}
+
+
